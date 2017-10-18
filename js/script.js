787107f0c51ad5c1dcdb5d45767eec1f1a48c89c
andy.vilsack <andy.vilsack@gmail.com>
2017-10-18 13:35:00 -0400

    js stuff

---
+++ js/script.js
@@ -0,0 +1,15 @@
+boardArray[82] = new Array;
+function rollDice()
+{
+	randomNumber = Math.floor(Math.random() * (7 - 1)) + 1;
+	console.log(randomNumber);
+	movePlayer();
+}
+function movePlayer(playerID)
+{
+
+	
+
+
+	
+}
